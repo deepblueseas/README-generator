f88ed02b7a2b4b15c6dec0f7e1a7701f720fa760
Tess McGovern <ren.mcgovern@gmail.com>
2024-07-13 20:42:49 -0400

    started index.js worked on prompts and the init function

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,39 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const { writeFile } = require('fs').promises;
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter your project title',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project',
+    },
+    {
+
+    }
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then((answers) => writeFile('README.md', generateMarkdown(answers)))
+    .then(() => console.log('Successfully wrote to README.md'))
+    .catch((err) => console.error(err));
+ };
 
 // Function call to initialize app
 init();
